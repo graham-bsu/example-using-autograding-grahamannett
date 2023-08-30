c02a3cf0912c373ea7184129ed09f2cbd928f481
graham <graham.annett@gmail.com>
2023-08-30 16:45:09 -0600

    commit

---
+++ OtherTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CacheTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
